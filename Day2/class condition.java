a5555b67cb0eeaa37a9ad692a3ce874cf4ad1115
kripasagar3 <kripasagarsivakumar@gmail.com>
2024-05-20 18:12:37 +0530

    Add files via upload

---
+++ Day2/class condition.java
@@ -0,0 +1,12 @@
+class condition
+{
+    public static void main(String[] args )
+    {
+        int x=18;
+        
+        if(x>=18)
+        {
+            System.out.println("you are eligible");
+        }
+    }
+}
