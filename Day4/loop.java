1edb1e0e410b172ace95eda45fa5c077ceb0fc34
kripasagar3 <kripasagarsivakumar@gmail.com>
2024-05-20 16:24:32 +0530

    Create loop.java

---
+++ Day4/loop.java
@@ -0,0 +1,21 @@
+class loop
+{
+    public static void main(String args[])
+         {
+            int stu=10;
+            while(stu<100){
+                if (stu%2==0)
+                {
+                    System.out.println("even ones"+stu);
+                }
+                else{
+                    System.out.println("odd once"+stu);
+                }
+
+            //System.out.println(stu);
+            
+            stu+=10;
+            }
+
+         }
+}
