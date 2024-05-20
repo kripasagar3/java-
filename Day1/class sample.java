acee9f65414f1b57e56c561b915e86f12c268a1d
kripasagar3 <kripasagarsivakumar@gmail.com>
2024-05-20 18:20:38 +0530

    Create class sample.java

---
+++ Day1/class sample.java
@@ -0,0 +1,18 @@
+
+
+class demod2
+{
+    public static void main(String args[])
+    {
+        sample obj=new sample("harish",20,"mani");
+    }
+}
+class sample
+{
+    sample(String name,int age,String place)
+    {
+        System.out.println("hi how are you"+name);
+        System.out.println("hi how are you"+age);
+        System.out.println("hi how are you"+place);
+    }
+}
