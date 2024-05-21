98fbba4a0713ce060c20824ec7dcfed6d83fb562
kripasagar3 <kripasagarsivakumar@gmail.com>
2024-05-21 20:00:30 +0530

    Add files via upload

---
+++ Day1/bank.java
@@ -0,0 +1,45 @@
+class loan
+{
+    public static void main(String args[])
+    {
+        bank obj=new bank();
+        
+    }
+
+}
+class bank
+  {
+    bank()
+    {
+    byte age=30;
+    boolean job=false;
+    boolean securities=true;
+    boolean asset=true;
+    boolean  dependent=false;
+    if((age>=25) && (age<=65))
+    {
+        System.out.println("your age is eligible ");
+        if((securities==true) && (job==true))
+          {
+            System.out.println("you are eligible for loan");
+          }
+          else{
+            System.out.println("you need more requirements for loan");
+
+          }
+    }
+    else if(age<25)
+    {
+        System.out.println("you are eligible for student loan");
+    }
+    else if((age>65) && (dependent==true))
+    {
+        System.out.println("you are eligible for pensioners loan");
+    }
+    else 
+    {
+        System.out.println("you are not eligible for loan");
+    }
+    }
+  }
+
